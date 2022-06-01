3e6840df7f5b2901d10095aa05c3c15763d16c9a
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2022-06-01 22:38:50 +0300

    Add files via upload

---
+++ Reactor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Reactor.hpp"
+
+
+ReactorPtr create_reactor() {
+    ReactorPtr reactor =(ReactorPtr)malloc(sizeof(Reactor));
+    reactor->id = 0;
+    reactor->callback = NULL;
+    reactor->thread = 0;
+    return reactor;
+}
+// this function is called by the reactor thread
+void InstallHandler(ReactorPtr reactor, int file_num, void *(*callback)(void *)) {
+    reactor->id = file_num;
+    reactor->callback = callback;
+    ReactorManagerPtr manager = (ReactorManagerPtr)malloc(sizeof(ReactorManager));
+    manager->reactor_count = file_num;
+    manager->reactorPointer = reactor;
+    pthread_create(&reactor->thread, NULL, callback, reactor);
+}
+
+// This function is responsible for removing the handler from the reactor.
+void removeHandler(ReactorPtr reactor, int file_num) {
+    pthread_cancel(reactor->thread); // maybe change  it pthread join
+    reactor->id = -1;
+    reactor->callback = NULL;
+    
+}
---
+++ Reactor.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <pthread.h>
+
+typedef struct Reactor {
+    int id;
+    void *(*callback)(void *);
+    pthread_t thread;
+
+}Reactor, *ReactorPtr;
+
+typedef struct ReactorManager {
+    ReactorPtr reactorPointer;
+    int reactor_count;
+}ReactorManager, *ReactorManagerPtr;
+
+ReactorPtr create_reactor();
+void InstallHandler(ReactorPtr reactor, int file_num, void *(*callback)(void *));
+void removeHandler(ReactorPtr reactor, int file_num);
+
+
